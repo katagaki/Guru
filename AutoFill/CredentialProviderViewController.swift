06d3ecf399f6c8158a84e49942e441e8f1d39fa8
katagaki <68231035+katagaki@users.noreply.github.com>
2021-08-08 17:14:57 +0800

    Update AutoFill provider.

--- AutoFill/CredentialProviderViewController.swift
+++ AutoFill/CredentialProviderViewController.swift
@@ -7,9 +7,13 @@
 
 import AuthenticationServices
 import LocalAuthentication
+import os
 import UIKit
 
 class CredentialProviderViewController: ASCredentialProviderViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var blurView: UIView!
+    @IBOutlet weak var cancelButton: StylizedButton!
     
     let defaults = UserDefaults.standard
     let laContext = LAContext()
@@ -17,7 +21,9 @@ class CredentialProviderViewController: ASCredentialProviderViewController, UITa
     var autofillLogs: String = "Guru AutoFill Provider Extension"
     
     var userProfile: UserProfile?
-    var logins: [Login] = []
+    var scopedLogins: [Login] = []
+    
+    var selectedAccountName: String = ""
     
     @IBOutlet weak var tableView: UITableView!
     
@@ -26,91 +32,176 @@ class CredentialProviderViewController: ASCredentialProviderViewController, UITa
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        tableView.contentInset.bottom = blurView.frame.height
+        tableView.verticalScrollIndicatorInsets.bottom = blurView.frame.height
+        
         // Localization
-        navigationItem.title = NSLocalizedString("Logins", comment: "Views")
+        cancelButton.setTitle(NSLocalizedString("Cancel", comment: "General"), for: .normal)
+        
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        unlock()
-        if let userProfile = userProfile {
-            logins = userProfile.logins
+        if userProfile == nil {
+            unlock()
+            if let userProfile = userProfile {
+                if selectedAccountName != "" {
+                    if let login = userProfile.login(withName: selectedAccountName),
+                       let password = login.password {
+                        let passwordCredential = ASPasswordCredential(user: login.username ?? "", password: password)
+                        self.extensionContext.completeRequest(withSelectedCredential: passwordCredential, completionHandler: nil)
+                    }
+                } else {
+                    tableView.reloadData()
+                }
+            } else {
+                extensionContext.cancelRequest(withError:
+                                                NSError(domain: ASExtensionErrorDomain,
+                                                        code: ASExtensionError.userCanceled.rawValue))
+            }
+        } else {
+            tableView.reloadData()
+            
         }
-        tableView.reloadData()
     }
     
     // MARK: Interface Builder
     
     @IBAction func cancel(_ sender: AnyObject?) {
-        self.extensionContext.cancelRequest(withError: NSError(domain: ASExtensionErrorDomain, code: ASExtensionError.userCanceled.rawValue))
+        extensionContext.cancelRequest(withError:
+                                        NSError(domain: ASExtensionErrorDomain,
+                                                code: ASExtensionError.userCanceled.rawValue))
     }
     
     // MARK: ASCredentialProviderViewController
     
     override func prepareCredentialList(for serviceIdentifiers: [ASCredentialServiceIdentifier]) {
         log("prepareCredentialList called.")
+        unlock()
         if let userProfile = userProfile {
-            logins = userProfile.logins.filter({ login in
-                for serviceIdentifier in serviceIdentifiers {
-                    if serviceIdentifier.type == .URL, let loginURL = login.loginURL, loginURL.contains(serviceIdentifier.identifier) {
-                        return true
+            
+            // Add scoped logins
+            if serviceIdentifiers.count > 0 {
+                log("Preparing scoped login list for \(serviceIdentifiers[0].identifier).")
+                scopedLogins = userProfile.logins.filter({ login in
+                    for serviceIdentifier in serviceIdentifiers {
+                        if let loginURL = login.loginURL, (loginURL.contains(serviceIdentifier.identifier) || serviceIdentifier.identifier.contains(loginURL)) {
+                            return true
+                        }
                     }
+                    return false
+                })
+                log("Number of scoped logins: \(scopedLogins.count).")
+            }
+            
+            // Reload credential store
+            var identities: [ASPasswordCredentialIdentity] = []
+            
+            for login in userProfile.logins {
+                if let accountName = login.accountName, let loginURL = login.loginURL, let username = login.username {
+                    let url: String = (loginURL.starts(with: "https://") || loginURL.starts(with: "http://") ? loginURL : "https://" + loginURL)
+                    let identity: ASPasswordCredentialIdentity = ASPasswordCredentialIdentity(serviceIdentifier: ASCredentialServiceIdentifier(identifier: url, type: .URL), user: username, recordIdentifier: accountName)
+                    identities.append(identity)
                 }
-                return false
-            })
+            }
+            
+            ASCredentialIdentityStore.shared.replaceCredentialIdentities(with: identities) { bool, error in
+                if let error = error {
+                    self.log("ASCredentialIdentityStore replace completed with error: \(error.localizedDescription).")
+                } else {
+                    self.log("ASCredentialIdentityStore replace completed.")
+                }
+            }
         }
     }
-
-    /*
-     Implement this method if your extension supports showing credentials in the QuickType bar.
-     When the user selects a credential from your app, this method will be called with the
-     ASPasswordCredentialIdentity your app has previously saved to the ASCredentialIdentityStore.
-     Provide the password by completing the extension request with the associated ASPasswordCredential.
-     If using the credential would require showing custom UI for authenticating the user, cancel
-     the request with error code ASExtensionError.userInteractionRequired.
-     */
+    
     override func provideCredentialWithoutUserInteraction(for credentialIdentity: ASPasswordCredentialIdentity) {
         log("provideCredentialWithoutUserInteraction called.")
-        if let userProfile = userProfile {
-            // credentialIdentity.serviceIdentifier.identifier
-            let passwordCredential = ASPasswordCredential(user: "j_appleseed", password: "apple1234")
-            self.extensionContext.completeRequest(withSelectedCredential: passwordCredential, completionHandler: nil)
-        } else {
-            self.extensionContext.cancelRequest(withError: NSError(domain: ASExtensionErrorDomain, code:ASExtensionError.userInteractionRequired.rawValue))
-        }
+        extensionContext.cancelRequest(withError:
+                                        NSError(domain: ASExtensionErrorDomain,
+                                                code: ASExtensionError.userInteractionRequired.rawValue))
     }
-
-    /*
-     Implement this method if provideCredentialWithoutUserInteraction(for:) can fail with
-     ASExtensionError.userInteractionRequired. In this case, the system may present your extension's
-     UI and call this method. Show appropriate UI for authenticating the user then provide the password
-     by completing the extension request with the associated ASPasswordCredential.
-     */
+    
     override func prepareInterfaceToProvideCredential(for credentialIdentity: ASPasswordCredentialIdentity) {
         log("prepareInterfaceToProvideCredential called.")
+        selectedAccountName = credentialIdentity.recordIdentifier!
     }
     
     // MARK: UITableViewDataSource, UITableViewDelegate
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return logins.count
+        if let userProfile = userProfile {
+            if scopedLogins.count == 0 {
+                return userProfile.logins.count
+            } else {
+                switch section {
+                case 0: return scopedLogins.count
+                case 1: return userProfile.logins.count
+                default: return 0
+                }
+            }
+        } else {
+            return 0
+        }
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
+        if scopedLogins.count == 0 {
+            return 1
+        } else {
+            return 2
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if userProfile == nil {
+            return ""
+        }
+        if scopedLogins.count == 0 {
+            return NSLocalizedString("AutoFillLogins", comment: "AutoFill")
+        } else {
+            switch section {
+            case 0: return NSLocalizedString("AutoFillSuggestedLogins", comment: "AutoFill")
+            case 1: return NSLocalizedString("AutoFillLogins", comment: "AutoFill")
+            default: return ""
+            }
+        }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "AccountCell") as! DetailWithImageCell
-        cell.iconView.image = userProfile!.logins[indexPath.row].accountIcon()
-        cell.titleLabel.text = userProfile!.logins[indexPath.row].accountName
-        cell.subtitleLabel.text = userProfile!.logins[indexPath.row].username
-        return cell
+        if scopedLogins.count > 0 && indexPath.section == 0 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "AccountCell") as! DetailWithImageCell
+            cell.iconView.image = scopedLogins[indexPath.row].accountIcon()
+            cell.titleLabel.text = scopedLogins[indexPath.row].accountName
+            cell.subtitleLabel.text = scopedLogins[indexPath.row].username
+            return cell
+        } else {
+            if let userProfile = userProfile {
+                let cell = tableView.dequeueReusableCell(withIdentifier: "AccountCell") as! DetailWithImageCell
+                cell.iconView.image = userProfile.logins[indexPath.row].accountIcon()
+                cell.titleLabel.text = userProfile.logins[indexPath.row].accountName
+                cell.subtitleLabel.text = userProfile.logins[indexPath.row].username
+                return cell
+            } else {
+                return UITableViewCell()
+            }
+        }
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let passwordCredential = ASPasswordCredential(user: logins[indexPath.row].username ?? "", password: logins[indexPath.row].password ?? "")
-        self.extensionContext.completeRequest(withSelectedCredential: passwordCredential, completionHandler: nil)
+        switch indexPath.section {
+        case 0:
+            let passwordCredential = ASPasswordCredential(user: scopedLogins[indexPath.row].username ?? "",
+                                                          password: scopedLogins[indexPath.row].password ?? "")
+            self.extensionContext.completeRequest(withSelectedCredential: passwordCredential, completionHandler: nil)
+        case 1:
+            if let userProfile = userProfile {
+                let passwordCredential = ASPasswordCredential(user: userProfile.logins[indexPath.row].username ?? "",
+                                                              password: userProfile.logins[indexPath.row].password ?? "")
+                self.extensionContext.completeRequest(withSelectedCredential: passwordCredential, completionHandler: nil)
+            }
+        default: break
+        }
         tableView.deselectRow(at: indexPath, animated: true)
     }
     
@@ -140,9 +231,7 @@ class CredentialProviderViewController: ASCredentialProviderViewController, UITa
     }
     
     func log(_ text: String) {
-        let dateString = String(Date().timeIntervalSince1970).components(separatedBy: ".")[0]
-        autofillLogs = "\(autofillLogs)\n[\(dateString)] AUTOFILL: \(text)"
-        print(text)
+        os_log("%s", log: .default, type: .info, text)
     }
     
 }
